ec4a55435a3097c2d9c1846fff2aba4e9fdbb43a
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-08 17:44:41 -0400

    Fix error with viewport not being used

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-    }),
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+    })
   ],
   module: {
     rules: [
